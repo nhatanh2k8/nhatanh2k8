3341d5bd207a09a7aef6ac1ac8b0869e62f1759e
nhatanh2k8 <164532068+nhatanh2k8@users.noreply.github.com>
2025-07-08 00:41:46 +0700

    Update tds.py

--- tds.py
+++ tds.py
@@ -1,25 +1,5 @@
-import importlib.util
-import traceback
-import os
 
-# Đường dẫn tới file main.py nằm cùng thư mục
-file_path = os.path.join(os.path.dirname(__file__), 'main.py')
-module_name = 'main_module'
+from functions.main import Main
 
-try:
-    # Load file main.py như một module
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    # Gọi hàm Main() nếu tồn tại
-    if hasattr(module, 'Main'):
-        module.Main()
-    else:
-        print("⚠️ Không tìm thấy hàm Main trong main.py")
-
-except Exception as e:
-    print("❌ Đã xảy ra lỗi:")
-    traceback.print_exc()
-
-input("\n👉 Nhấn Enter để thoát...")
+if __name__ == "__main__":
+    Main()
